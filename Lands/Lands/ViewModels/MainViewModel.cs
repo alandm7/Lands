d9dc46dfa1961f13e89aba4b81b479d9ce714ba9
AlanDeMiguel <alandemiguel@gmail.com>
2018-05-24 12:24:15 -0500

    Login View Model

---
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Lands.ViewModels
+{
+    public class MainViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+          get;
+          set;
+        }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+
+        #endregion
+    }
+}
